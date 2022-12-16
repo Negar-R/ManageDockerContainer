8eeb0a2233d7a160850e897856f52cf0a96ab79e
Negar <negar.r.78.ioi@gmail.com>
2022-12-16 22:13:42 +0330

    Create a model App and register it in admin

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
-from django.db import models
+from rest_framework import serializers
+
+from .models import App
+
+
+class ManageAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = ["id", "name", "image", "command", "envs", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at"]
