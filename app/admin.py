7d66d7950cceb166341e5814b333cf8161a0d7f7
Negar <negar.r.78.ioi@gmail.com>
2022-12-16 22:24:58 +0330

    Add a new model to store docker container changes
    - register this model in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import App
+from .models import App, AppContainerHistory
 
 # Register your models here.
 
 admin.site.register(App)
+admin.site.register(AppContainerHistory)
--- app/models.py
+++ app/models.py
@@ -11,3 +11,21 @@ class App(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class AppContainerHistory(models.Model):
+    # TODO: set it to cascade
+    app = models.ForeignKey(App, null=True, on_delete=models.CASCADE)
+    # As App object can be updated and it is possible to change its props, so it is required to consider
+    # a new field for each of its props in this model.
+    container_short_id = models.CharField(max_length=12, unique=True)
+    container_name = models.CharField(max_length=255)
+    container_image = models.CharField(max_length=255)
+    container_command = models.CharField(max_length=255, null=True, blank=True)
+    container_envs = models.JSONField(default=dict)
+    container_status = models.CharField(max_length=100)
+    container_logs = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.container_name
