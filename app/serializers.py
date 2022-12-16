0193a8b80a0eaa8f1b4bd40ef68f3ecf4f0ab3df
Negar <negar.r.78.ioi@gmail.com>
2022-12-16 23:13:26 +0330

    Add a new feature to run app and create docker container
    - Use docker SDK in python to manage containers
    - Store some information about the container in DB
    - Write a new serializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import App
+from .models import App, AppContainerHistory
 
 
 class ManageAppSerializer(serializers.ModelSerializer):
@@ -8,3 +8,11 @@ class ManageAppSerializer(serializers.ModelSerializer):
         model = App
         fields = ["id", "name", "image", "command", "envs", "created_at", "updated_at"]
         read_only_fields = ["id", "created_at", "updated_at"]
+
+
+class AppContainerHistorySerializer(serializers.ModelSerializer):
+    app = serializers.CharField(source="app.name")
+
+    class Meta:
+        model = AppContainerHistory
+        fields = "__all__"
--- app/views.py
+++ app/views.py
@@ -4,10 +4,46 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
-from .models import App
-from .serializers import ManageAppSerializer
+from .models import App, AppContainerHistory
+from .serializers import ManageAppSerializer, AppContainerHistorySerializer
 
 
 class ManageAppView(viewsets.ModelViewSet):
     queryset = App.objects.all()
-    serializer_class = ManageAppSerializer
+    serializers = {
+        "default": ManageAppSerializer,
+        "run": AppContainerHistorySerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializers.get(self.action, self.serializers["default"])
+
+    @action(detail=True, methods=["get"], name="run")
+    def run(self, request, *args, **kwargs):
+        app = self.get_object()
+        try:
+            client = docker.from_env()
+            container = client.containers.run(
+                image=app.image,
+                command=app.command,
+                environment=app.envs,
+                detach=True,
+            )
+            app_container_log = AppContainerHistory.objects.create(
+                app=app,
+                container_short_id=container.short_id,
+                container_name=container.name,
+                container_image=container.image,
+                container_command=app.command,
+                container_envs=app.envs,
+                # TODO: Be more precise about the status
+                container_status=container.status,
+                container_logs=container.logs(),
+            )
+            serializer = self.get_serializer(app_container_log)
+            return Response(
+                serializer.data,
+                headers={"message": "container runs successfully"},
+            )
+        except Exception as e:
+            return Response(e)
