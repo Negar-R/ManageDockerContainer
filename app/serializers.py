79586280ab09b936998acb07eceec01a70480b31
Negar <negar.r.78.ioi@gmail.com>
2022-12-17 15:07:32 +0330

    Add status to serializer and calculate it on the fly

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,10 @@
+import docker
+
 from rest_framework import serializers
 
-from .models import App, AppContainerHistory
+from .models import App, AppContainerHistory, ContainerStatus
+
+client = docker.from_env()
 
 
 class ManageAppSerializer(serializers.ModelSerializer):
@@ -12,6 +16,15 @@ class ManageAppSerializer(serializers.ModelSerializer):
 
 class AppContainerHistorySerializer(serializers.ModelSerializer):
     app = serializers.CharField(source="app.name")
+    status = serializers.SerializerMethodField()
+
+    def get_status(self, container_history) -> ContainerStatus:
+        container = client.containers.get(container_history.container_short_id)
+        return (
+            ContainerStatus.RUNNING.name
+            if container.status in ["created", "running", "restarting"]
+            else ContainerStatus.FINISHED.name
+        )
 
     class Meta:
         model = AppContainerHistory
